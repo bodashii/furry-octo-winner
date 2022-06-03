52e99258486e09ba3304460a8fef785462009e20
maddnty <nzkb24@gmail.com>
2022-06-02 21:59:16 -0500

    added db.sql and schema for tables

---
+++ db/db.sql
@@ -0,0 +1,4 @@
+DROP DATABASE IF EXISTS furry_octo_winner_db;
+
+CREATE DATABASE furry_octo_winner_db;
+USE furry_octo_winner_db;
---
+++ db/schema.sql
@@ -0,0 +1,19 @@
+CREATE TABLE department (
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(30)
+);
+
+CREATE TABLE role (
+    id INTEGER PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL,
+    department_id INTEGER
+);
+
+CREATE TABLE employee (
+    id INT PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT,
+    manager_id INT NULL
+);
