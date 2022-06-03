962b2ea7356896e3a4dee1dd9b1b194047784247
maddnty <nzkb24@gmail.com>
2022-06-02 23:05:59 -0500

    added get requests for department and delete for department

--- server.js
+++ server.js
@@ -1,10 +1,75 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'qwerty',
+        database: 'furry_octo_winner_db'
+    },
+    console.log('Connected to the furry_octo_winner_db database')
+);
+
+// Get all departments
+app.get('/api/department', (req, res) => {
+    const sql = `SELECT * FROM department`;
+
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message});
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: rows
+        });
+    });
+});
+
+app.get('/api/department/:id', (req, res) {
+    const sql = `SELECT * FROM department WHERE id = ?`;
+    const params = [req.params.id];
+
+    db.query(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
+});
+
+// delete a department
+app.delete('/api/department/:id', (req, res) => {
+    const sql = `DELETE FROM department WHERE id ?`;
+    const params = [req.params.id];
+
+    db.query(sq., params, (err, result) => {
+        if (err) {
+            res.statusMessage(400).json({ error: res.message });
+        } else if (!result.affectedRows) {
+            res.json({
+                message: 'Candidate not found'
+            });
+        } else {
+            res.json({
+                message: 'deleted',
+                changes: result.affectedRows,
+                id: req.params.id
+            });
+        }
+    });
+});
 
 app.get('/', (req, res) => {
     res.json({
